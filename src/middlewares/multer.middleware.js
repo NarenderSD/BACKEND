9451f2480ce83a5f63af75af069d4b31ba7ee529
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-03-18 11:39:27 +0530

    multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
+
+
+// Disk stroage is used to store the file on the disk
+
+//public ki temp folder me store hoga file
